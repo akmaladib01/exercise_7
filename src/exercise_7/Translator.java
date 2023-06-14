7ed005b9ff139c3429e911201c82350c9b19af3a
akmaladib01 <akmaladib15801@gmail.com>
2023-06-15 05:24:12 +0800

    exercise_7

---
+++ src/exercise_7/Translator.java
@@ -0,0 +1,14 @@
+package exercise_7;
+
+public class Translator {
+
+    public static String translate(String word, String targetLanguage) {
+
+        if (targetLanguage.equals("Bahasa Melayu")) {
+            return "Selamat Pagi";
+        } else {
+            return "The language is not available";
+        }
+
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Asus
+ *
+ */
+module exercise_7 {
+	requires java.desktop;
+}
